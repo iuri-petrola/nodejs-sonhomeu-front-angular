0c28e7020ddf75eebd0f664b298423397d5d2682
Iuri Petrola <iuri.petrola@gmail.com>
2025-12-07 16:49:42 -0300

    Ajuste apontamento do back

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
-    apiUrl: 'https://sonhomeuloja.com/api',
+    //apiUrl: 'https://sonhomeuloja.com/api',
+    apiUrl: 'http://nodejs-someu-back:8080/',
     imgUrl: 'files'
 }
